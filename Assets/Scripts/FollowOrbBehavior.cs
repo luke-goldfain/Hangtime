e73513306538d84c235bac8a27363e7aa15b6f4e
luke-goldfain <awsumluke22@gmail.com>
2019-08-25 19:47:18 -0500

    Made follow orb change direction based on altitude, small changes to prefabs and Player scripts

--- Assets/Scripts/FollowOrbBehavior.cs
+++ Assets/Scripts/FollowOrbBehavior.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 
 public class FollowOrbBehavior : MonoBehaviour
 {
+    public float maxSpeed;
+    public float minSpeed;
+    private float speed;
+    private float speedToLerp;
+
     public List<Vector3> CheckpointPositions;
-    private int currentCheckptTarget;
+    public int CurrentCheckptTarget;
+
+    private float currentHeight;
+    private float prevHeight;
+
+    private Rigidbody rb;
 
     public GameObject CheckpointPrefab;
     public GameObject FinishZonePrefab;
@@ -19,20 +29,33 @@ public class FollowOrbBehavior : MonoBehaviour
     void Start()
     {
         finishPlaced = false;
+
+        rb = GetComponent<Rigidbody>();
+
+        speed = minSpeed;
+        speedToLerp = maxSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = Vector3.Lerp(this.transform.position, CheckpointPositions[currentCheckptTarget], 0.01f);
+        speed = Mathf.Lerp(speed, speedToLerp, 0.1f);
 
-        if (Vector3.Distance(this.transform.position, CheckpointPositions[currentCheckptTarget]) <= 5f)
+        UpdateMovePreferringHighAltitude();
+
+        if (Vector3.Distance(this.transform.position, CheckpointPositions[CurrentCheckptTarget]) <= 40f &&
+            Vector3.Distance(this.transform.position, CheckpointPositions[CurrentCheckptTarget]) > 2f)
         {
-            if (CheckpointPositions.Count > currentCheckptTarget + 1)
+            speedToLerp = minSpeed;
+        }
+
+        if (Vector3.Distance(this.transform.position, CheckpointPositions[CurrentCheckptTarget]) <= 2f)
+        {
+            if (CheckpointPositions.Count > CurrentCheckptTarget + 1)
             {
                 DropCheckpoint();
 
-                currentCheckptTarget++;
+                CurrentCheckptTarget++;
             }
             else if (!finishPlaced)
             {
@@ -40,14 +63,84 @@ public class FollowOrbBehavior : MonoBehaviour
 
                 finishPlaced = true;
             }
+            
+            speedToLerp = maxSpeed;
         }
+    }
+
+    private void UpdateMovePreferringHighAltitude()
+    {
+        Vector3 moveTarget = CheckpointPositions[CurrentCheckptTarget];
+
+        prevHeight = currentHeight;
+
+        // Dear god. So this basically raycasts downwards from four points in the cardinal directions from the follow orb.
+        // If the orb detects that it's moving uphill, it will correct to a direction wherein the raycast down returns a 
+        // higher distance, meaning the orb will trend towards a higher altitude.
+        if (Physics.Raycast(this.transform.position + (Vector3.down * 4), Vector3.down, out RaycastHit hit, Mathf.Infinity, CheckpointMask))
+        {
+            currentHeight = Vector3.Distance(this.transform.position, hit.point);
+
+            if (currentHeight < prevHeight)
+            {
+                Vector3 tallestHeight = CheckpointPositions[CurrentCheckptTarget]; // Default to continuing to move to checkpoint
+                float tempCurrentHeight = currentHeight;
+
+                if (Physics.Raycast(this.transform.position + (Vector3.left * 4) + (Vector3.down * 4), Vector3.down, out RaycastHit leftHit, Mathf.Infinity, CheckpointMask))
+                {
+                    if (Vector3.Distance(this.transform.position + (Vector3.left * 4) + (Vector3.down * 4), leftHit.point) > tempCurrentHeight &&
+                        Vector3.Distance(this.transform.position + (Vector3.left * 4), CheckpointPositions[CurrentCheckptTarget]) < Vector3.Distance(this.transform.position, CheckpointPositions[CurrentCheckptTarget]))
+                    {
+                        tallestHeight = this.transform.position + (Vector3.left * 4);
+
+                        tempCurrentHeight = Vector3.Distance(tallestHeight, leftHit.point);
+                    }
+                }
+
+                if (Physics.Raycast(this.transform.position + (Vector3.right * 4) + (Vector3.down * 4), Vector3.down, out RaycastHit rightHit, Mathf.Infinity, CheckpointMask))
+                {
+                    if (Vector3.Distance(this.transform.position + (Vector3.right * 4) + (Vector3.down * 4), rightHit.point) > tempCurrentHeight &&
+                        Vector3.Distance(this.transform.position + (Vector3.right * 4), CheckpointPositions[CurrentCheckptTarget]) < Vector3.Distance(this.transform.position, CheckpointPositions[CurrentCheckptTarget]))
+                    {
+                        tallestHeight = this.transform.position + (Vector3.right * 4);
+
+                        tempCurrentHeight = Vector3.Distance(tallestHeight, rightHit.point);
+                    }
+                }
+
+                if (Physics.Raycast(this.transform.position + (Vector3.forward * 4) + (Vector3.down * 4), Vector3.down, out RaycastHit forwardHit, Mathf.Infinity, CheckpointMask))
+                {
+                    if (Vector3.Distance(this.transform.position + (Vector3.forward * 4) + (Vector3.down * 4), forwardHit.point) > tempCurrentHeight &&
+                        Vector3.Distance(this.transform.position + (Vector3.forward * 4), CheckpointPositions[CurrentCheckptTarget]) < Vector3.Distance(this.transform.position, CheckpointPositions[CurrentCheckptTarget]))
+                    {
+                        tallestHeight = this.transform.position + (Vector3.forward * 4);
+
+                        tempCurrentHeight = Vector3.Distance(tallestHeight, forwardHit.point);
+                    }
+                }
+
+                if (Physics.Raycast(this.transform.position + (Vector3.back * 4) + (Vector3.down * 4), Vector3.down, out RaycastHit backHit, Mathf.Infinity, CheckpointMask))
+                {
+                    if (Vector3.Distance(this.transform.position + (Vector3.back * 4) + (Vector3.down * 4), backHit.point) > tempCurrentHeight &&
+                        Vector3.Distance(this.transform.position + (Vector3.back * 4), CheckpointPositions[CurrentCheckptTarget]) < Vector3.Distance(this.transform.position, CheckpointPositions[CurrentCheckptTarget]))
+                    {
+                        tallestHeight = this.transform.position + (Vector3.back * 4);
+
+                        tempCurrentHeight = Vector3.Distance(tallestHeight, backHit.point);
+                    }
+                }
+
+                moveTarget = tallestHeight;
+            }
+        }
+
+        this.transform.position = Vector3.MoveTowards(this.transform.position, moveTarget, speed);
     }
 
     private void DropCheckpoint()
     {
-        RaycastHit hit;
 
-        if (Physics.Raycast(this.transform.position + (Vector3.down * 4), Vector3.down, out hit, Mathf.Infinity, CheckpointMask))
+        if (Physics.Raycast(this.transform.position + (Vector3.down * 4), Vector3.down, out RaycastHit hit, Mathf.Infinity, CheckpointMask))
         {
             Instantiate(CheckpointPrefab, hit.point, Quaternion.identity);
         }
@@ -55,9 +148,8 @@ public class FollowOrbBehavior : MonoBehaviour
 
     private void DropFinishZone()
     {
-        RaycastHit hit;
 
-        if (Physics.Raycast(this.transform.position + (Vector3.down * 4), Vector3.down, out hit, Mathf.Infinity, CheckpointMask))
+        if (Physics.Raycast(this.transform.position + (Vector3.down * 4), Vector3.down, out RaycastHit hit, Mathf.Infinity, CheckpointMask))
         {
             Instantiate(FinishZonePrefab, hit.point, Quaternion.identity);
         }
---
+++ Assets/Scripts/Player/RopeSectionPositioner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeSectionPositioner : MonoBehaviour
+{
+    private Vector3 playerPos, grapplePos;
+    public int SectionNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPos = GetComponentInParent<PlayerController>().transform.position;
+
+        grapplePos = GetComponentInParent<PlayerController>().GrappleHitPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = GetComponentInParent<PlayerController>().transform.position;
+        
+        grapplePos = GetComponentInParent<PlayerController>().GrappleHitPosition;
+        
+        Vector3 avgPosition = (grapplePos + playerPos) / 2;
+        Vector3 avgPSide = (avgPosition + playerPos) / 2;
+        Vector3 avgGSide = (avgPosition + grapplePos) / 2;
+
+        switch (SectionNumber % 3)
+        {
+            case 0:
+                this.transform.position = avgPosition;
+                break;
+            case 1:
+                this.transform.position = avgPSide;
+                break;
+            case 2:
+                this.transform.position = avgGSide;
+                break;
+        }
+    }
+}
