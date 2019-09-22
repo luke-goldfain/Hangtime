5d9f1ed190f7bfc8c9de334adce83b987005b4bd
DutaiStudios <44248998+DutaiStudios@users.noreply.github.com>
2019-09-22 04:47:29 -0500

    Objectives
    Objective System and UI Setup w/ Multi-Objective functionality.

---
+++ Assets/Scripts/TargetManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+    public Transform playerReference;
+    public Image indicatorReference;
+    public List<Transform> targetList;
+
+    private Vector3 indicatorVector;
+    private Text textReference;
+    private int targetRange;
+    private int targetIndex;
+    private bool targetValidated;
+
+
+    void Awake()
+    {
+        
+        textReference = indicatorReference.GetComponentInChildren<Text>();
+    }
+
+    void Start()
+    {
+        
+        targetIndex = 0;
+        targetRange = 5;
+        
+        targetValidated = targetList.Count > 0;
+    }
+
+    void LateUpdate()
+    {
+        
+        if (targetValidated)
+        {
+            if (LinearDistance(playerReference.position, targetList[targetIndex].position) < targetRange)
+            {
+                targetIndex = (targetIndex + 1) % targetList.Count;
+            }
+           
+            UpdateTargetSystem(targetIndex);
+        }
+    }
+
+    public void UpdateTargetSystem(int index)
+    {
+        if (targetValidated)
+        {
+          
+            indicatorReference.gameObject.SetActive(RelativePosition(playerReference, targetList[index]));
+            
+            if (targetList[index].gameObject.activeInHierarchy)
+            {
+               
+                textReference.text = LinearDistance(playerReference.position, targetList[index].position) + "m";
+                
+                indicatorVector = indicatorReference.rectTransform.anchorMin;
+                indicatorVector.x = Camera.main.WorldToViewportPoint(targetList[index].position).x;
+                indicatorReference.rectTransform.anchorMin = indicatorVector;
+                indicatorReference.rectTransform.anchorMax = indicatorVector;
+            }
+        }
+    }
+   
+    public int LinearDistance(Vector3 playerPosition, Vector3 targetPosition)
+    {
+        
+        playerPosition.y = 0;
+        targetPosition.y = 0;
+        
+        return Mathf.RoundToInt(Vector3.Distance(playerPosition, targetPosition));
+    }
+   
+    private bool RelativePosition(Transform player, Transform target)
+    {
+        
+        return Vector3.Dot(Vector3.forward, player.InverseTransformPoint(target.position).normalized) > 0;
+    }
+}
