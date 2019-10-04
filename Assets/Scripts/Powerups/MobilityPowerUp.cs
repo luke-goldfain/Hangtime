5195268879c7d880506452028d9164b8a400800b
Tim <timothy.c13@gmail.com>
2019-10-03 23:13:42 -0500

    PowerUp (WIP)
    +Added PowerUps script
    +Added MobilityPowerUp script.

---
+++ Assets/Scripts/Powerups/MobilityPowerUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobilityPowerUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void ActivePowerUp(Collider player)
+    {
+        PlayerController stats = player.GetComponent<PlayerController>();
+        stats.DefaultRunSpeed = 30;
+        stats.JumpForce = 20;
+    }
+
+
+}
