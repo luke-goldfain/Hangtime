8ba87d85240a25e93f4814595842ec8b63f20514
Tim <timothy.c13@gmail.com>
2019-10-31 16:26:37 -0500

    Added Characters
    - Pinky Model added to project.
    - Pinky Player prefab created.
    - Pinky Animator set up.
    - Pinky Textures Applied.
    - Songbird Textures added to project.
    - Songbird Model added to project.

---
+++ Assets/Scripts/AnimCharacterController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the animations of character
+/// </summary>
+public class AnimCharacterController : MonoBehaviour
+{
+    public float speed;
+    public float gravity;
+    Vector3 moveDir = Vector3.zero;
+
+      CharacterController controller;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController> ();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveDir = new Vector3(0, 0, 1);
+
+            }
+        }
+    }
+}
